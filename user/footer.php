f3c580ee9ebd567d04f2373a29ce46f264bcfae2
habibeu38 <habib.eu.38@gmail.com>
2025-11-30 10:06:50 +0600

    Add files via upload

---
+++ user/footer.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<style type="text/css">
+	footer
+	{
+		height: 180px;
+		width: device width;
+		background-color: green;
+	}
+		.fa
+		{
+			margin: 0px 5px;
+			padding: 5px;
+			font-size: 20px;
+			width: 15px;
+			height: 15px;
+			text-align: center;
+			text-decoration: none;
+			border-radius: 50%;
+		}
+		.fa:hover
+		{
+			opacity: .7;
+		}
+		.fa-facebook
+		{
+			background: #3B5998;
+			color: white;
+		}
+		.fa-twitter
+		{
+			background: #55ACEE;
+			color: white;
+		}
+		}
+		.fa-instagram
+		{
+			background: #125688;
+			color: white;
+		}
+	</style>
+
+</head>
+<body>
+<footer style="background-color: green; ">
+	<br>
+	<h3 style="color:white;text-align: center;">Contact us through social media</h3><br>
+
+	<div style="margin:0px 570px;">
+
+		<a href="#" class="fa fa-facebook"></a>
+		<a href="#" class="fa fa-twitter"></a>
+		<a href="#" class="fa fa-instagram"></a>
+	</div>
+
+	<br>
+	<p style="color:white;text-align: center;">
+		<br>
+		Email:&nbsp habib.eu.38@gmail.com <br><br>
+		Mobile:&nbsp 01793720929
+	</p>
+</footer>
+</body>
+</html>
